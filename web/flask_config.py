d6adc666bdceb0c5adeea8ccf3bffa5fb8dc3d4d
Kent <kent05011607@gmail.com>
2021-05-28 04:24:31 +0800

    split services into frontend, backend, and reverse proxy

--- web/flask_config.py
+++
@@ -1,6 +0,0 @@
-import secrets
-
-DEBUG = True
-TEMPLATES_AUTO_RELOAD = True
-
-SECRET_KEY = secrets.token_bytes(32)
--- web/pages.py
+++
@@ -1,9 +0,0 @@
-import flask
-
-
-blueprint = flask.Blueprint('indexpage', __name__)
-
-
-@blueprint.route('/')
-def index_page():
-    return flask.render_template('index.html')
